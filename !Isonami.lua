d131551dfaf6b0acd5434785592025339f4e3d62
渟雲 <M3351AN@outlook.jp>
2022-01-25 16:59:19 +0800

    Update !Isonami.lua

--- !Isonami.lua
+++ !Isonami.lua
@@ -11,6 +11,66 @@ ffi = require 'ffi'
 ffi.cdef[[
     typedef uintptr_t (__thiscall* GetClientEntity_4242425_t)(void*, int);
 
+    typedef struct
+    {
+        float x;
+        float y;
+        float z;
+    } Vector_t;
+
+    typedef struct
+    {
+        char        pad0[0x60]; // 0x00
+        void*       pEntity; // 0x60
+        void*       pActiveWeapon; // 0x64
+        void*       pLastActiveWeapon; // 0x68
+        float        flLastUpdateTime; // 0x6C
+        int            iLastUpdateFrame; // 0x70
+        float        flLastUpdateIncrement; // 0x74
+        float        flEyeYaw; // 0x78
+        float        flEyePitch; // 0x7C
+        float        flGoalFeetYaw; // 0x80
+        float        flLastFeetYaw; // 0x84
+        float        flMoveYaw; // 0x88
+        float        flLastMoveYaw; // 0x8C // changes when moving/jumping/hitting ground
+        float        flLeanAmount; // 0x90
+        char        pad1[0x4]; // 0x94
+        float        flFeetCycle; // 0x98 0 to 1
+        float        flMoveWeight; // 0x9C 0 to 1
+        float        flMoveWeightSmoothed; // 0xA0
+        float        flDuckAmount; // 0xA4
+        float        flHitGroundCycle; // 0xA8
+        float        flRecrouchWeight; // 0xAC
+        Vector_t    vecOrigin; // 0xB0
+        Vector_t    vecLastOrigin;// 0xBC
+        Vector_t    vecVelocity; // 0xC8
+        Vector_t    vecVelocityNormalized; // 0xD4
+        Vector_t    vecVelocityNormalizedNonZero; // 0xE0
+        float        flVelocityLenght2D; // 0xEC
+        float        flJumpFallVelocity; // 0xF0
+        float        flSpeedNormalized; // 0xF4 // clamped velocity from 0 to 1
+        float        flRunningSpeed; // 0xF8
+        float        flDuckingSpeed; // 0xFC
+        float        flDurationMoving; // 0x100
+        float        flDurationStill; // 0x104
+        bool        bOnGround; // 0x108
+        bool        bHitGroundAnimation; // 0x109
+        char        pad2[0x2]; // 0x10A
+        float        flNextLowerBodyYawUpdateTime; // 0x10C
+        float        flDurationInAir; // 0x110
+        float        flLeftGroundHeight; // 0x114
+        float        flHitGroundWeight; // 0x118 // from 0 to 1, is 1 when standing
+        float        flWalkToRunTransition; // 0x11C // from 0 to 1, doesnt change when walking or crouching, only running
+        char        pad3[0x4]; // 0x120
+        float        flAffectedFraction; // 0x124 // affected while jumping and running, or when just jumping, 0 to 1
+        char        pad4[0x208]; // 0x128
+        float        flMinBodyYaw; // 0x330
+        float        flMaxBodyYaw; // 0x334
+        float        flMinPitch; //0x338
+        float        flMaxPitch; // 0x33C
+        int            iAnimsetVersion; // 0x340
+    } CCSGOPlayerAnimationState_534535_t;
+
     struct WeaponInfo_t
     {
         char _0x0000[20];
@@ -104,7 +164,9 @@ local at_targets_only_in_air = ui.add_check_box("at targets only in air", "at_ta
 
 local slowwalk_breaker = ui.add_key_bind("slowwalk breaker", "slowwalk_breaker", 0,1)
 
+local leg_fucker = ui.add_check_box("leg breaker", "leg_fucker", false)
 
+local sta_leg = ui.add_check_box("static leg", "sta_leg", false)
 --VISUALS
 --=========================================================================================================================
 local molo_color = ui.add_color_edit("MolotovRadius", "molo_color", true, color_t.new(255, 255, 255, 255))
@@ -122,7 +184,7 @@ local hotkey_binds_y = ui.add_slider_int("hotkey_binds_y", "hotkey_binds_y", 0, 
 
 --MISC
 --=========================================================================================================================
-local ragdoll = ui.add_check_box("ragdoll", "ragdoll", false)
+local ragdoll = ui.add_check_box("ragdoll gravity", "ragdoll", false)
 local kill_say =  ui.add_check_box("kill say", "kill_say", false)
 
 
@@ -140,6 +202,14 @@ for i = 0, 64 do player_shots[i] = 0.0 end
 
 local players_dormant = {}
 for i = 0, 64 do players_dormant[i] = 0.0 end
+
+local entity_list_ptr = ffi.cast("void***", se.create_interface("client.dll", "VClientEntityList003"))
+local get_client_entity_fn = ffi.cast("GetClientEntity_4242425_t", entity_list_ptr[0][3])
+
+local offset_value = 0x9960
+local shared_onground
+
+local m_flPoseParameter = se.get_netvar("DT_BaseAnimating", "m_flPoseParameter")
 
 local m_vecOrigin = se.get_netvar("DT_BaseEntity", "m_vecOrigin")
 local m_flDuckSpeed = se.get_netvar("DT_BasePlayer", "m_flDuckSpeed");
@@ -451,7 +521,63 @@ client.register_callback("paint", on_legit_aa)
 client.register_callback('unload', on_unload_legit_aa)
 client.register_callback("paint", set_speed)
 --=========================================================================================================================
+local function for_break(cmd) 
+	local localplayer = entitylist.get_local_player()
+	if not localplayer then return end
+    ffi.cast("float*", ffi_helpers.get_entity_address(localplayer:get_index()) + 10100)[0] = 0
+end
 
+function leg_breaker(cmd)  
+if leg_fucker:get_value() == true then
+
+        local localplayer = entitylist.get_local_player()
+        if not localplayer then return end
+        ffi.cast("float*", ffi_helpers.get_entity_address(localplayer:get_index()) + m_flPoseParameter)[0] = 0
+        local antihit_extra_leg_movement = ui.get_combo_box("antihit_extra_leg_movement")
+        if clientstate.get_choked_commands() == 0 then
+            antihit_extra_leg_movement:set_value(2)
+        else
+            antihit_extra_leg_movement:set_value(1)
+        end
+
+end
+end
+
+client.register_callback("paint", for_break)
+client.register_callback("paint", leg_breaker)
+
+function for_static()
+    local localplayer = entitylist.get_local_player()
+    if not localplayer then return end
+    local m_fFlags = se.get_netvar("DT_BasePlayer", "m_fFlags")
+
+    local bOnGround = bit.band(localplayer:get_prop_float(m_fFlags), bit.lshift(1,0)) ~= 0
+    if not bOnGround then
+        ffi.cast("CCSGOPlayerAnimationState_534535_t**", ffi_helpers.get_entity_address(localplayer:get_index()) + offset_value)[0].flDurationInAir = 99
+        ffi.cast("CCSGOPlayerAnimationState_534535_t**", ffi_helpers.get_entity_address(localplayer:get_index()) + offset_value)[0].flHitGroundCycle = 0
+        ffi.cast("CCSGOPlayerAnimationState_534535_t**", ffi_helpers.get_entity_address(localplayer:get_index()) + offset_value)[0].bHitGroundAnimation = false
+    end
+
+    shared_onground = bOnGround
+end
+
+client.register_callback("paint", for_static)
+
+function on_sta_leg()
+if sta_leg:get_value() == true then
+    local localplayer = entitylist.get_local_player()
+    if not localplayer then return end
+
+    local m_fFlags = se.get_netvar("DT_BasePlayer", "m_fFlags")
+
+    local bOnGround = bit.band(localplayer:get_prop_float(m_fFlags), bit.lshift(1,0)) ~= 0
+    if bOnGround and not shared_onground then 
+        ffi.cast("CCSGOPlayerAnimationState_534535_t**", ffi_helpers.get_entity_address(localplayer:get_index()) + offset_value)[0].flDurationInAir = 0.5
+    end -- ACT_CSGO_LAND_LIGHT
+end
+end
+client.register_callback("paint", on_sta_leg)
+--=========================================================================================================================
 function on_low_delta_on_slowwalk()
     local_player = entitylist:get_local_player()
 
